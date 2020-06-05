38aa416ffc8ecf5797fc6a793e01ecc99dbb5fad
roberthaworth1234 <roberthaworth1234@hotmail.com>
2020-06-05 13:37:24 +0100

    two more tests added

--- src/utils.js
+++ src/utils.js
@@ -2,10 +2,11 @@ exports.calculateCost = (array, pricingStructure) => {
   if (!array.length) {
     return 0;
   } else {
-    // if item is special priced it will be placed in this object
+    // if item is special priced it will be placed in this object with object entries of special deal
     let specialPriceItems = {};
     // if item is not spicial priced it will be added to the the cost below
-    let singlePricedItemsCost = 0;
+    let totalCost = 0;
+    // an object to track the total number of each special item
     let totalSpecials = {};
     array.forEach(item => {
       if (pricingStructure[item].hasOwnProperty("specialPrice")) {
@@ -18,17 +19,24 @@ exports.calculateCost = (array, pricingStructure) => {
           totalSpecials[item] = 1;
         }
       } else {
-        singlePricedItemsCost += pricingStructure[item].unitPrice;
+        totalCost += pricingStructure[item].unitPrice;
       }
     });
     Object.keys(specialPriceItems).forEach(item => {
       if (totalSpecials[item] === 1) {
-        singlePricedItemsCost += pricingStructure[item].unitPrice;
+        totalCost += pricingStructure[item].unitPrice;
       }
       if (totalSpecials[item] % specialPriceItems[item][0] === 0) {
-        singlePricedItemsCost += specialPriceItems[item][1];
+        totalCost += specialPriceItems[item][1];
+      }
+      if (
+        totalSpecials[item] % specialPriceItems[item][0] !== 0 &&
+        totalSpecials[item] > 1
+      ) {
+        totalCost +=
+          pricingStructure[item].unitPrice + specialPriceItems[item][1];
       }
     });
-    return singlePricedItemsCost;
+    return totalCost;
   }
 };
--- src/utils.test.js
+++ src/utils.test.js
@@ -30,4 +30,15 @@ describe("calculateCost", () => {
     );
     expect(input).toEqual(115);
   });
+  it("will take an array containing multiple items, two of which trigger the special price.  Will produce correct calulated cost", () => {
+    const input = calculateCost(
+      ["A", "A", "A", "B", "B", "C", "D"],
+      refPricingStructure
+    );
+    expect(input).toEqual(210);
+  });
+  it("will take an array containing multiple items, one of which is larger than the required number in the special deal.  Will produce the correct result factoring in the special and unit price.", () => {
+    const input = calculateCost(["A", "B", "B", "B", "C"], refPricingStructure);
+    expect(input).toEqual(145);
+  });
 });
